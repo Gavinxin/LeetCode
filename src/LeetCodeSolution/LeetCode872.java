bee531ee8cb43983f0ab7be736c1a9b20655c5f2
Gavinxin <1822172738@qq.com>
2020-04-17 19:04:34 +0800

    2020/04/17

--- src/LeetCodeSolution/LeetCode872.java
+++ src/LeetCodeSolution/LeetCode872.java
@@ -1,5 +1,31 @@
 package LeetCodeSolution;
 
-public class LeetCode872 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LeetCode872 {
+	/**
+	 * 872. 叶子相似的树 请考虑一颗二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+	 * 
+	 * @param root1
+	 * @param root2
+	 * @return
+	 */
+	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+		List<Integer> list1 = new ArrayList<Integer>();
+		List<Integer> list2 = new ArrayList<Integer>();
+		dfs(root1, list1);
+		dfs(root2, list2);
+		return list1.equals(list2);
+	}
+
+	private void dfs(TreeNode root1, List<Integer> list1) {
+		if(root1!=null) {
+			if(root1.left == null &&root1.right == null)
+				list1.add(root1.val);
+			dfs(root1.left,list1);
+			dfs(root1.right,list1);
+		}
+
+	}
 }
--- src/LeetCodeSolution/LeetCode88.java
+++ src/LeetCodeSolution/LeetCode88.java
@@ -1,5 +1,17 @@
 package LeetCodeSolution;
 
-public class LeetCode88 {
+import java.util.Arrays;
 
+public class LeetCode88 {
+	/**
+	 * 
+	 * @param nums1
+	 * @param m
+	 * @param nums2
+	 * @param n
+	 */
+	 public void merge(int[] nums1, int m, int[] nums2, int n) {
+		 System.arraycopy(nums2, 0, nums1, m, n);
+		 Arrays.sort(nums1);
+	 }
 }
