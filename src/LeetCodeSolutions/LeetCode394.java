622e5d010ea49906509b867ade4f3069478590ed
Gavinxin <1822172738@qq.com>
2020-05-28 18:45:44 +0800

    20200528

--- src/LeetCodeSolutions/LeetCode394.java
+++ src/LeetCodeSolutions/LeetCode394.java
@@ -1,6 +1,7 @@
 package LeetCodeSolutions;
 
 import java.util.Stack;
+import java.util.function.BinaryOperator;
 
 public class LeetCode394 {
 	/**
@@ -24,14 +25,26 @@ public class LeetCode394 {
 				}
 				stemp = stemp.reverse();
 				stack.pop();
-				int f = Integer.valueOf(stack.pop());
+				// consider mul number
+				StringBuilder num= new StringBuilder();
+				while(stack.size()>0&&Character.isDigit(stack.peek().charAt(0))) {
+					num.insert(0,stack.pop());
+				}
+				int f = Integer.valueOf(num.toString());
 				StringBuilder t= new StringBuilder();
 				for (int j = 0; j < f; j++) {
 					t.append(stemp);
 				}
 				stack.add(t.toString());
 				i++;
-				System.out.println(stack.size());
+				stack.stream().reduce("", new BinaryOperator<String>() {
+					
+					@Override
+					public String apply(String arg0, String arg1) {
+						// TODO Auto-generated method stub
+						return arg0+arg1;
+					}
+				}).toString();
 			}
 			
 		}
@@ -42,6 +55,6 @@ public class LeetCode394 {
     }
 	public static void main(String[] args) {
 		LeetCode394 l = new LeetCode394();
-		System.out.println(l.decodeString("3[a]2[bc]"));
+		System.out.println(l.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"));
 	}
 }
