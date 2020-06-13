2a015dd1d487cf22b2f8e025ad2829693fdb6280
Gavinxin <1822172738@qq.com>
2020-06-13 19:02:38 +0800

    20200613

---
+++ src/LeetCodeSolutions/LeetCode207.java
@@ -0,0 +1,43 @@
+package LeetCodeSolutions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LeetCode207 {
+	/**
+	 * 207. ¿Î³Ì±í
+	 * 
+	 * @param numCourses
+	 * @param prerequisites
+	 * @return
+	 */
+
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		int[] indegrees = new int[numCourses];
+		List<List<Integer>> adjacency = new ArrayList<>();
+		Queue<Integer> queue = new LinkedList<>();
+		for (int i = 0; i < numCourses; i++)
+			adjacency.add(new ArrayList<>());
+		// Get the indegree and adjacency of every course.
+		for (int[] cp : prerequisites) {
+			indegrees[cp[0]]++;
+			adjacency.get(cp[1]).add(cp[0]);
+		}
+		// Get all the courses with the indegree of 0.
+		for (int i = 0; i < numCourses; i++)
+			if (indegrees[i] == 0)
+				queue.add(i);
+		// BFS TopSort.
+		while (!queue.isEmpty()) {
+			int pre = queue.poll();
+			numCourses--;
+			for (int cur : adjacency.get(pre))
+				if (--indegrees[cur] == 0)
+					queue.add(cur);
+		}
+		return numCourses == 0;
+	}
+
+}
