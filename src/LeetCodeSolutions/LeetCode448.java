6a198c944dea9b7bb4a390f6f86d87880a81c051
Gavinxin <1822172738@qq.com>
2020-06-21 17:43:41 +0800

    20200621

---
+++ src/LeetCodeSolutions/LeetCode448.java
@@ -0,0 +1,29 @@
+package LeetCodeSolutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode448 {
+	/**
+	 * 448. 找到所有数组中消失的数字
+	 * @param nums
+	 * @return
+	 */
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			nums[nums[i]-1] = Math.abs(nums[nums[i]-1])*-1;
+			
+		}
+		List<Integer> res = new ArrayList<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if(nums[i]>0) 
+				res.add(i+1);
+		}
+		return res;
+    }
+	public static void main(String[] args) {
+		int x = 2;
+		x *=-1;
+		System.out.println(x);
+	}
+}
